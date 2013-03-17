f7375a8c48a42cdb2baf78ace508c1c11e42e4ef
Ian Watt <ian@whatswatt.net>
2013-03-17 23:09:51 +0000

    Dosage completed.  bat file added for demo dosage run via changing duration to 1 minutes within the demo account. bat file also added for normal cycle depended on hourly time stated in dosage1/2/3.

--- application/models/dispenser_model.php
+++ application/models/dispenser_model.php
@@ -23,9 +23,10 @@ class Dispenser_Model extends CI_Model
 			$data = $disp_insert;
 			$this->db->insert('TBdispenser', $data);
 		}
-		//lets kick of the dispenser run through
-		function disp_run()
+		//lets kick of the dispenser run through in minutes
+		function disp_run_demo()
 			{
+	
 				//lets create the table that we need  for doing the run through of prescriptions
 				$this->db->select('*');
 				$this->db->where('PREstatus', '0');
@@ -49,6 +50,78 @@ class Dispenser_Model extends CI_Model
 				foreach ($pre_actives->result() as $pre_active)
 					{
 						if($datetime == $pre_active->PREstartdate)
+							{
+								//lets load the ssh3 helper
+								$this->load->helper('SSH2.php');
+								
+								//lets get the ip of the dispenser and the authenicationd etails
+								$disp_ip = $pre_active->DISip;
+								$disp_username = $pre_active->DISusername;
+								$disp_password = $pre_active->DISpassword;
+								
+								//lets create the details for the SSH connection
+								$ssh = new NET_SSH2($disp_ip);
+								if (!$ssh->login($disp_username, $disp_password)) 
+									{
+										//exit if you cannot get logged on
+										exit('Login Failed');
+									}
+
+								//lets work out which motor we are suppose to be connecting to
+								if ($pre_active->premotor = '1') 
+									{
+										//command we want to send to the dispenser
+										$ssh->exec('sudo python pimotor1.py');
+										echo '<br>';
+										echo 'motor 1 success';
+										echo '<br>';
+									}
+								elseif ($pre_active->premotor = '2')
+									{
+										//command we want to send to the dispenser
+										$ssh->exec('killall -v apt-get');
+									}
+								elseif ($pre_active->premotor = '3')
+									{
+										//command we want to send to the dispenser
+										$ssh->exec('killall -v apt-get');
+									}
+									echo 'completed';
+							}
+						else
+							{
+								//do nothing and move onto the next record
+								echo 'failure';
+							}
+					}
+			}
+		//lets kick of the dispenser run through
+		function disp_run()
+			{
+	
+				//lets create the table that we need  for doing the run through of prescriptions
+				$this->db->select('*');
+				$this->db->where('PREstatus', '0');
+				$this->db->from('TBprescription');
+				$this->db->join('TBdispenser', 'TBprescription.PREdispenserid = TBdispenser.DISid', 'INNER');
+				$pre_actives = $this->db->get();
+				
+				//print_r ($pre_actives->result_array());
+				
+				//lets get the current data stamp
+				$time_hour = date('H');
+				$time_minute = date('i');
+				$time_round = $time_minute;
+				$time_final = "$time_hour:$time_minute";
+				$date = date('Y-m-d');
+				$datetime = "$date $time_final";
+				
+				print_r($datetime);
+
+				//lets get to work on the running through the results from the table created
+				foreach ($pre_actives->result() as $pre_active)
+					{
+						if($timefinal == $pre_active->PREdosage1 || $timefinal == $pre_active->dosage2 || $timefinal == $pre_active->dosage3)
 							{
 								//lets load the ssh3 helper
 								$this->load->helper('SSH2.php');
